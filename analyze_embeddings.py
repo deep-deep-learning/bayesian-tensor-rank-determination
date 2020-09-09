f56b8e083f38d896a5b474626c9dfc2a3eb361b1
colehawkins <colepshawkins@gmail.com>
2020-09-09 15:16:32 -0700

    Add clustering

--- analyze_embeddings.py
+++ analyze_embeddings.py
@@ -1,7 +1,7 @@
 #%%
 import torch
 
-i = 2
+i = 3
 
 fp = 'weights/emb%s.pt'%i
 
@@ -13,6 +13,13 @@ from sklearn.cluster import KMeans
 import time
 t = time.time()
 predicted = KMeans(n_clusters = 200).fit_predict(emb.cpu().detach().numpy())
+
+
+
 print("Total time ",time.time()-t)
 #%%
-200*250*250
+[200*250*250]
+
+fp = 'weights/clusterings/emb%s.npz'%i
+import numpy as np
+np.save(fp,predicted)
