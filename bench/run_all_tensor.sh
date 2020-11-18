4c69574b156621a1a1627e3e66d61ca7bcd2429e
colehawkins <colepshawkins@gmail.com>
2020-11-18 15:22:46 -0800

    Add extra stepsize

--- bench/run_all_tensor.sh
+++ bench/run_all_tensor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for tensor_type in "CP" "TensorTrain" "TensorTrainMatrix" "Tucker"
 do for no_kl_steps in 50000 100000 150000;
-do for lr in 1.0 0.1 0.01;
+do for lr in 1.0 0.1 0.01 0.001;
 do
 	name="${tensor_type}_warmup_${no_kl_steps}_adagrad_${lr}"
 	./bench/tensorized_dlrm.sh --optimizer='Adagrad' --learning-rate=0.1 --save-model="saved_models/${name}" --tensor-type="${tensor_type}" --use-gpu=1 --test-freq=10240 --print-freq=1024 --kl-multiplier=1.0 --no-kl-steps=${no_kl_steps} > logs/${name}.log
