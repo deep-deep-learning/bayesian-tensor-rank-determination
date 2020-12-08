392840a7fddce7445dda04487db281097b7ba089
colehawkins <colepshawkins@gmail.com>
2020-12-08 19:17:23 +0000

    Update scripts with proper minibatch size

--- bench/run_ttm.sh
+++ bench/run_ttm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 for tensor_type in "TensorTrainMatrix" 
-do for kl_mult in 0.001;
+do for kl_mult in 0.005 0.001 0.0005;
 do for no_kl_steps in 50000;
-do for lr in 0.005;
+do for lr in 0.005 0.001;
 do
-	export CUDA_VISIBLE_DEVICES=1
+	export CUDA_VISIBLE_DEVICES=0
 	name="${tensor_type}_warmup_${no_kl_steps}_${optimizer}_lr_${lr}_kl_${kl_mult}"
-	./bench/tensorized_dlrm.sh  --nepochs=3 --optimizer="Adam" --learning-rate=${lr} --save-model="saved_models/${name}" --tensor-type="${tensor_type}" --use-gpu=1 --test-freq=10240 --print-freq=1024 --kl-multiplier=${kl_mult} --no-kl-steps=${no_kl_steps} > logs/${name}.log
+	./bench/tensorized_dlrm.sh  --mini-batch-size=512 --nepochs=3 --optimizer="Adam" --learning-rate=${lr} --save-model="saved_models/${name}" --tensor-type="${tensor_type}" --use-gpu=1 --test-freq=10240 --print-freq=1024 --kl-multiplier=${kl_mult} --no-kl-steps=${no_kl_steps} > logs/${name}.log
 done
 done
 done
