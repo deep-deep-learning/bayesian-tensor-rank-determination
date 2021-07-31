a9a9751684dcad7ed7c5317c0ea5b799e03bf21f
Ubuntu <ubuntu@ip-172-31-41-59.us-east-2.compute.internal>
2021-07-31 19:10:54 +0000

    update hpo scripts

--- nlp/scripts/ttm.sh
+++ nlp/scripts/ttm.sh
@@ -5,7 +5,7 @@ BATCH_SIZE=256
 
 
 for tensor_type in 'TensorTrainMatrix';
-do for kl_mult in 0.0 1-e1 1e-2 1e-4 1e-5;
+do for kl_mult in 0.0 1e-4 5e-5 1e-5 5e-6 1e-6;
 do python train.py  --n_epochs=100 --embedding ${tensor_type} --rank 20 --lr 0.0005 --kl-multiplier $kl_mult --rank-loss True --batch-size $BATCH_SIZE | tee logs/${tensor_type}_low_batch_${kl_mult}.txt;
 done
 done
