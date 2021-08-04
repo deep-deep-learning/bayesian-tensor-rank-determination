41d2644956de6c5f77209c832f89c061f3c6e038
Ubuntu <ubuntu@ip-172-31-41-59.us-east-2.compute.internal>
2021-08-04 21:28:46 +0000

    scripts finalized

--- mnist/scripts/cp.sh
+++ mnist/scripts/cp.sh
@@ -4,7 +4,7 @@
 
 
 for tensor_type in  'CP';
-do for kl_mult in 1e-3 5e-4 1e-4 5e-5 1e-5;
+do for kl_mult in 5e-5;
 do python train.py   --model-type ${tensor_type} --rank 50  --kl-multiplier $kl_mult --rank-loss True | tee logs/${tensor_type}_${kl_mult}.txt;
 done
 done
